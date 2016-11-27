bc2316ba517ed9550a6f038873977e25d5d870cb
darthbeep <darthbeep@gmail.com>
2016-11-26 21:39:20 -0500

    You can run commands multiple times.

--- execute.c
+++ execute.c
@@ -2,18 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 void input(){
+  pid_t past = getpid();
+  int status;
+  fork();
+  wait(&status);
+  //printf("PPid: %d, Pid: %d\n", getppid(), getpid());
+  if (getppid() == past) {
+
+
   printf("Enter command: ");
-  char *a = calloc(1,255); 
+  char *a = calloc(1,255);
   fgets(a, 255, stdin);
   a = strsep(&a, "\n"); //Remove newline since "the newline is retained."
   //printf("%s\n", a);
-  
+
   char *s;
   char *ret[20];
   int i = 0;
-  
+
   while(a){
     s = strsep(&a, " ");
     //printf("%s\n", s);
@@ -22,21 +31,21 @@ void input(){
     i++;
   }
   ret[i] = 0;
-  
+
   free(a);
 
   execvp(ret[0], ret);
-  
+}
 }
 
 
 int main(){
 
-  while (1){
+for (size_t i = 0; i < 10; i++) {
+  input();
+}
 
-      input();
-      
-    }
-  
+
+
+
   return 0;}
-
