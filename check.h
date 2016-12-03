beee5bb810a89f0a80c06490ba76573d8b3c8570
MatthewSo <MatthewSo@Matthews-MacBook-Air.local>
2016-12-02 20:58:59 -0500

    header

--- check.h
+++ check.h
@@ -1,12 +1,6 @@
 
-#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 #include "execute.h"
 #ifndef CHECK
 #define CHECK
