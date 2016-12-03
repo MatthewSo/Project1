132d19afe46d193f799a99070e09934c5b39fcc0
MatthewSo <MatthewSo@Matthews-MacBook-Air.local>
2016-12-02 20:43:01 -0500

    Breaking c files

---
+++ check.c
@@ -0,0 +1,238 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "execute.c"
+
+        /*======== int checkCD() ==========
+	Inputs:  char * com[]
+
+	Returns: 1 if first slot contains "cd" and the code is run. Else, 0
+
+        Checks to see whether or not the user input uses the
+	"cd" command by looking for "cd" in com[]. If so, changes the location
+	based on the input in com[]. Returns 1 if sucessful.
+	Returns 0 if not.
+	====================*/
+
+
+int checkCD(char * com[]) {
+  if (strcmp(com[0], "cd") == 0) {
+    if (com[1] == NULL) {
+      path = backup;
+      locations[0] = "~";
+      locnum = 0;
+      chdir(getenv("HOME"));
+    }
+    else {
+      path = (char *) malloc(sizeof(char) + sizeof(com[1]) + 1);
+      strcat(path, com[1]);
+      strcat(path, "/");
+      if (strcmp(com[1], "..") == 0) {
+        locnum--;
+      }
+      else {
+	locnum++;
+	locations[locnum] = com[1];
+      }
+      int chd = chdir(path);
+      if (chd == -1) {
+	locnum--;
+      }
+    }
+    return 1;
+  }
+  return 0;
+}
+
+        /*======== int checkGreaterRedirect() ==========
+	Inputs:  char * input
+	         int past
+
+	Returns: 1 if first slot contains ">" and the code is run. Else, 0
+
+        Checks to see whether or not the user input uses
+	">" by looking for "<" in com[]. If so, directs the stdout of the command (on left)
+	into the file given (on right). Returns 1 if sucessful.
+	Returns 0 if not.
+	====================*/
+
+int checkGreaterRedirect(char * input, int past){
+    if (strchr(input, '>') == NULL) {
+    return 0;
+  }
+  else {
+    char *s;
+    char *coms[20];
+    int end = 0;
+    while(input){
+      s = strsep(&input, ">");
+      coms[end] = s;
+      end++;
+    }
+    int k = 0;
+    while (coms[1][k] == ' ') {
+      coms[1]++;
+    }
+    if (coms[0][strlen(coms[0])-1]==' ') {
+      coms[0][strlen(coms[0])-1] = 0;
+    }
+
+    FILE *fp;
+    fp = fopen(coms[1], "w");
+    FILE* p = popen(coms[0], "r");
+    if (!p) return 1;
+
+    char buff[1024];
+    while (fgets(buff, sizeof(buff), p)) {
+      printf("");
+            fprintf(fp, "%s", buff);
+    }
+    fclose(fp);
+    pclose(p);
+
+      return 1;
+  }
+  }
+
+/*int checkLesserRedirect(char * input, int path) {
+  if (strchr(input)) {
+    return 0;
+  }
+  return 1;
+}*/
+        /*======== int checkLesserRedirect() ==========
+	Inputs:  char * input
+	         int past
+
+	Returns: 1 if first slot contains "<" and the code is run. Else, 0
+
+        Checks to see whether or not the user input uses
+	"<" by looking for "<" in com[]. If so, directs the file (right) as the stdin
+	for the command given (left). Returns 1 if sucessful.
+	Returns 0 if not.
+	====================*/
+
+int checkLesserRedirect(char * input, int past){
+  if (strchr(input, '<') == NULL) {
+    return 0;
+  }
+  else {
+    char *s;
+    char *coms[20];
+    int end = 0;
+    while(input){
+      s = strsep(&input, "<");
+      coms[end] = s;
+      end++;
+    }
+
+    FILE *f = fopen(coms[1], "rb");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    char *string = malloc(fsize + 1);
+    fread(string, fsize, 1, f);
+    string[fsize] = 0;
+    printf("%s\n",string);
+    fclose(f);
+
+
+
+      return 1;
+  }
+  }
+
+
+	/*======== int checkSemi() ==========
+	Inputs:  char * input
+	         int past
+
+	Returns: 1 if no ';' is present in input. Else, returns 0
+
+        If ';' are present, parses input at instances of ';'. It runs all parsed
+	components and returns 1;
+	If none are present, returns 0.
+
+	====================*/
+
+  int checkPipe(char * input, int past) {
+    if (strchr(input, '|') == NULL) {
+      return 0;
+    }
+    char *s;
+    char *coms[20];
+    int end = 0;
+
+    while(input){
+      s = strsep(&input, "|");
+      coms[end] = s;
+      end++;
+    }
+    if (coms[0][strlen(coms[0])-1] == ' ') {
+      coms[0][strlen(coms[0])-1] = 0;
+    }
+    if (coms[1][0] == ' ') {
+      coms[1]++;
+    }
+    fopen("verytemporaryfile", "w");
+    char * replace = "> verytemporaryfile";
+    char * new = (char *) malloc(sizeof(char));
+    strcat(new, coms[0]);
+    strcat(new, replace);
+    checkGreaterRedirect(new, past);
+    char * othernew= (char *) malloc(sizeof(coms[1]) + 20);
+    strcat(othernew, coms[1]);
+    strcat(othernew, " verytemporaryfile");
+    runstuff(othernew, past);
+    execlp("rm", "rm", "verytemporaryfile");
+    return 1;
+  }
+
+int checkSemi(char * input, int past) {
+  if (strchr(input, ';') == NULL) {
+    return 0;
+  }
+  else {
+    char *s;
+    char *coms[20];
+    int end = 0;
+
+    while(input){
+      s = strsep(&input, ";");
+      coms[end] = s;
+      end++;
+    }
+    coms[end] = 0;
+    for (size_t j = 0; j < end; j++) {
+      int k = 0;
+      while (coms[j][k] == ' ') {
+        coms[j]++;
+      }
+
+    }
+    for (size_t l = 0; l < end; l++) {
+        runstuff(coms[l], past);
+    }
+  }
+  return 1;
+}
+
+	/*======== int checkKill() ==========
+	Inputs:  char * com[]
+	Returns: 1 if exit is present in com[0] and the code is run. Else, 0.
+
+        Sees if com[0] is "exit." If it is, it kills the current process and returns 1.
+	Else, returns 0.
+	====================*/
+
+ int checkKill(char * com[]) {
+   if (strcmp(com[0], "exit") == 0) {
+    kill(getpid(),9);
+    return 1;}
+  return 0;}
---
+++ check.h
@@ -0,0 +1,10 @@
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include "execute.h"
